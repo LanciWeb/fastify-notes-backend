7b807281b22e27d4864db97b21d52eef373236d5
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-10-12 18:36:09 +0200

    Added notes controller

---
+++ src/controllers/notesController.js
@@ -0,0 +1,16 @@
+module.exports = {
+  //# create a note
+  create: async (request, reply) => {},
+
+  //#get the list of notes
+  fetch: async (request, reply) => {},
+
+  //#get a single note
+  get: async (request, reply) => {},
+
+  //#update a note
+  update: async (request, reply) => {},
+
+  //#delete a note
+  delete: async (request, reply) => {},
+};
--- src/routes/noteRoutes.js
+++ src/routes/noteRoutes.js
@@ -1,16 +1,18 @@
+const notesController = require('../controllers/notesController');
+
 module.exports = (app) => {
   //# create a note
-  app.post('/api/notes', (request, reply) => {});
+  app.post('/api/notes', notesController.create);
 
   //#get the list of notes
-  app.get('/api/notes', (request, reply) => {});
+  app.get('/api/notes', notesController.fetch);
 
   //#get a single note
-  app.get('/api/notes/:id', (request, reply) => {});
+  app.get('/api/notes/:id', notesController.get);
 
   //#update a note
-  app.put('/api/notes/:id', (request, reply) => {});
+  app.put('/api/notes/:id', notesController.update);
 
   //#delete a note
-  app.delete('/api/notes/:id', (request, reply) => {});
+  app.delete('/api/notes/:id', notesController.delete);
 };
