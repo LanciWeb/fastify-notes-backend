e310ae79fbec529860339c112f0036943d547cc0
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-10-12 18:44:41 +0200

    Added Fetch Notes endpoints

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -13,10 +13,25 @@ module.exports = {
   },
 
   //#get the list of notes
-  fetch: async (request, reply) => {},
+  fetch: async (request, reply) => {
+    try {
+      const notes = await Note.find({});
+      reply.code(200).send(notes);
+    } catch (e) {
+      reply.code(500).send(e);
+    }
+  },
 
   //#get a single note
-  get: async (request, reply) => {},
+  get: async (request, reply) => {
+    try {
+      const noteId = request.params.id;
+      const note = await Note.findById(noteId);
+      reply.code(200).send(note);
+    } catch (e) {
+      reply.code(500).send(e);
+    }
+  },
 
   //#update a note
   update: async (request, reply) => {},
